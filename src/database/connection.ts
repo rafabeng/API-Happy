0979fb69ae3902f042584072a4599f8f42f0c395
rafabeng <rafabeng@gmail.com>
2020-11-09 22:34:58 -0300

    ajuste

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,18 +1,3 @@
-import {ConnectionOptions} from 'typeorm';
+import {createConnection} from 'typeorm';
 
-const config: ConnectionOptions = {
-    type: 'postgres', 
-  host: process.env.TYPEORM_HOST, 
-  port: Number(process.env.TYPEORM_PORT), 
-  username: process.env.TYPEORM_USERNAME, 
-  password: process.env.TYPEORM_PASSWORD, 
-  database: process.env.TYPEORM_DATABASE,
-  entities: [
-        __dirname + '/../**/*.entity{.ts,.js}',
-  ], 
-  cli: {
-         migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
-       },    
-  };
-   
-  export = config;
+createConnection();
